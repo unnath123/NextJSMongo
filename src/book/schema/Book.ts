60d83e2a9410841d1a217466c280ad35bdba3eca
unnath123 <unnathpgowda@gmail.com>
2024-04-21 02:05:05 +0530

    basic structure

---
+++ src/book/schema/Book.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+export type BookDocument = Book & Document; 
+@Schema()
+export class Book{
+    @Prop()
+    title: string;
+
+    @Prop()
+    author: string;
+
+    @Prop()
+    published: number
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book)
